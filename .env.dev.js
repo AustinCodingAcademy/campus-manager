0a8fd37792768d3d033ed9a135b40089c39f295d
Canaan Davis <canaandavis@gmail.com>
2016-05-31 17:14:53 -0500

    Update .gitignore

--- .env.dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  GOOGLE_API_KEY:'AIzaSyD8nGoE2NLcDSBnIgTFvd_qrO1pULoxEy0',
-  GOOGLE_CLIENT_ID: '355169120001-23rnelkdfqjl43q0sllkffdth15eltgb.apps.googleusercontent.com',
-  YOUTUBE_CHANNEL_ID: 'UCzNpMM1lxoyj8paRCZoq5mA',
-  DOMAIN: 'http://localhost:3000',
-  MANDRILL_API_KEY: '',
-  MONGOLAB_URI: 'mongodb://localhost/aca-campus'
-};
