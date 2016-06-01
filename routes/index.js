d31e19380e1bb3b8185e2ea07a4027cfded270e3
Kevin Colten <kevincolten@gmail.com>
2016-06-01 17:24:58 -0500

    Added back in register

--- routes/index.js
+++ routes/index.js
@@ -35,62 +35,62 @@ router.get('/logout', function(req, res) {
   res.redirect('/');
 });
 
-// router.get('/register', function(req, res, next) {
-//   if(req.isAuthenticated()) {
-//     res.redirect('/');
-//   } else {
-//     res.render('register');
-//   }
-// });
-//
-// // Todo: We should probably look at extracting this into a module
-// router.post('/register', function(req, res, next) {
-//   UserModel.findOne({ username : req.body.username.toLowerCase() }, function (err, user) {
-//     var saltRounds = 10;
-//
-//     if (user) {
-//       req.flash('error', 'The email address has already been used');
-//       return res.redirect('/register');
-//     }
-//
-//     if (req.body.password.length <= 5) {
-//       req.flash('error', 'Password must be at least 6 characters');
-//       return res.redirect('/register');
-//     }
-//
-//     bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-//
-//       user = new UserModel({
-//         username: req.body.username.toLowerCase(),
-//         password: hash
-//       });
-//       user.is_client = true;
-//
-//       user.save(function (error, user) {
-//
-//         if (error) {
-//           req.flash('error', error.message);
-//           res.redirect('/register');
-//         }
-//
-//         user.client = user._id;
-//         user.save(function (error, user) {
-//
-//           if (error) {
-//             req.flash('error', error.message);
-//             res.redirect('/register');
-//           }
-//
-//           // If the users has been created successfully, log them in with
-//           // passport to start their session and redirect to the home route
-//           req.login(user, function(err) {
-//             if (err) { return res.redirect('/register'); }
-//             return res.redirect('/');
-//           });
-//         });
-//       });
-//     });
-//   });
-// });
+router.get('/register', function(req, res, next) {
+  if(req.isAuthenticated()) {
+    res.redirect('/');
+  } else {
+    res.render('register');
+  }
+});
+
+// Todo: We should probably look at extracting this into a module
+router.post('/register', function(req, res, next) {
+  UserModel.findOne({ username : req.body.username.toLowerCase() }, function (err, user) {
+    var saltRounds = 10;
+
+    if (user) {
+      req.flash('error', 'The email address has already been used');
+      return res.redirect('/register');
+    }
+
+    if (req.body.password.length <= 5) {
+      req.flash('error', 'Password must be at least 6 characters');
+      return res.redirect('/register');
+    }
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+
+      user = new UserModel({
+        username: req.body.username.toLowerCase(),
+        password: hash
+      });
+      user.is_client = true;
+
+      user.save(function (error, user) {
+
+        if (error) {
+          req.flash('error', error.message);
+          res.redirect('/register');
+        }
+
+        user.client = user._id;
+        user.save(function (error, user) {
+
+          if (error) {
+            req.flash('error', error.message);
+            res.redirect('/register');
+          }
+
+          // If the users has been created successfully, log them in with
+          // passport to start their session and redirect to the home route
+          req.login(user, function(err) {
+            if (err) { return res.redirect('/register'); }
+            return res.redirect('/');
+          });
+        });
+      });
+    });
+  });
+});
 
 module.exports = router;
