ff3214e20b4d111465d48668d38ba45e675a5ef2
Nick Dunn <nickdunn2@gmail.com>
2016-06-27 13:46:16 -0500

    CreateClient test asserts user email in body after registration

--- tests/01CreateClient.js
+++ tests/01CreateClient.js
@@ -3,12 +3,14 @@ module.exports = {
     browser
       .url('http://localhost:8080/register')
       .waitForElementVisible('body', 1000)
+      .setValue('input[name="first_name"]', 'Test')
+      .setValue('input[name="last_name"]', 'Client')
       .setValue('input[name="username"]', 'test@client.com')
       .setValue('input[name="password"]', 'testpw')
       .waitForElementVisible('button[type="submit"]', 1000)
       .click('button[type="submit"]')
       .pause(1000)
-      // .assert.containsText('.dropdown-button > span:nth-child(1)', 'test@client.com')
+      .assert.containsText('.card-content', 'test@client.com')
       .end();
   }
 }
