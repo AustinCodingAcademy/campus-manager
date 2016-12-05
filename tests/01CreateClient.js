a7f43af8a2255a9a4de2be3d8788f8e3905e1085
chrisperk <christopherperkins.512@gmail.com>
2016-12-05 16:28:17 -0600

    Create Term and Make Payment tests refactored

--- tests/01CreateClient.js
+++ tests/01CreateClient.js
@@ -2,11 +2,11 @@ module.exports = {
   'Create Client': function (browser) {
     browser
     .resizeWindow(1500, 1000)
-    .url('http://localhost:8080/register')
+    .url('http://localhost:3000/register')
     .waitForElementVisible('body', 1000)
     .setValue('input[name="first_name"]', 'Test')
-    .setValue('input[name="last_name"]', 'Client')
-    .setValue('input[name="username"]', 'test@client.com')
+    .setValue('input[name="last_name"]', 'User13')
+    .setValue('input[name="username"]', 'test@user13.com')
     .setValue('input[name="password"]', 'testpw')
     .waitForElementVisible('button[type="submit"]', 1000)
     .click('button[type="submit"]')
@@ -27,40 +27,6 @@ module.exports = {
     .pause(500)
   },
 
-  // 'Make Payment': function (browser) {
-  //   browser
-  //   .click('.brand-logo[href="#"]')
-  //   .pause(500)
-  //   .setValue('input', '200.00')
-  //   .pause(1000)
-  //   .click('[data-test="make-payment"]')
-  //   .pause(3000)
-  //   .keys('4242')
-  //   .pause(100)
-  //   .keys('4242')
-  //   .pause(100)
-  //   .keys('4242')
-  //   .pause(100)
-  //   .keys('4242')
-  //   .pause(500)
-  //   .keys('\uE004')
-  //   .pause(500)
-  //   .keys('11')
-  //   .keys('2020')
-  //   .pause(500)
-  //   .keys('\uE004')
-  //   .pause(500)
-  //   .keys('123')
-  //   .pause(500)
-  //   .keys('\uE004')
-  //   .pause(500)
-  //   .keys('77777')
-  //   .pause(500)
-  //   .keys('\uE007')
-  //   .pause(5000)
-  //   // Pause may need to be longer on other test clients
-  // },
-
   'Create Location': function (browser) {
     browser
     .click('[data-activates="nav-admin"]')
@@ -80,13 +46,12 @@ module.exports = {
     .keys('\uE004')
     .keys('Austin')
     .keys('\uE004')
-    .keys('Texas')
+    .keys('TX')
     .keys('\uE004')
     .keys('55555')
     .pause(100)
     .click('input[type="submit"]')
     .pause(1000)
-
   },
 
   'Create Term': function (browser) {
@@ -97,15 +62,24 @@ module.exports = {
     .pause(1000)
     .click('a[data-test="new-term"]')
     .pause(500)
-    .keys('\uE004')
-    .keys('test term')
-    .keys('\uE004')
+    .setValue('input[data-test="term-name"]', 'test term')
+    .click('input[data-test="term-start-date"]')
     .pause(500)
-    .keys('\uE007') // Presses ENTER btn
+    .click('div.picker--opened > div > div > div > div > div.picker__header > div.picker__nav--next')
     .pause(500)
-    .keys('\uE004')
+    .click('div.picker--opened > div > div > div > div > div.picker__header > div.picker__nav--next') //click next month twice so that term start is at least 30 days from today
     .pause(500)
-    .keys('\uE007') // Presses ENTER btn
+    .click('tr:nth-of-type(2) > td:first-of-type > div.picker__day')
+    .pause(1000)
+    .click('input[data-test="term-end-date"]')
+    .pause(1000)
+    .click('div.picker--opened > div > div > div > div > div.picker__header > div.picker__nav--next')
+    .pause(500)
+    .click('div.picker--opened > div > div > div > div > div.picker__header > div.picker__nav--next')
+    .pause(500)
+    .click('div.picker--opened > div > div > div > div > div.picker__header > div.picker__nav--next') //click next month 3x so that term end is at least 30 days from start date
+    .pause(500)
+    .click('div.picker--opened > div > div > div > div > table.picker__table > tbody > tr:nth-of-type(3) > td:last-of-type > div')
     .pause(500)
     .click('input[type="submit"]')
     .pause(1000);
@@ -122,7 +96,7 @@ module.exports = {
     .keys('\uE004')
     .keys('1234')
     .pause(500)
-    .keys('\uE004') // Presses ENTER btn
+    .keys('\uE004')
     .keys('12')
     .pause(500)
     .click('label[for="sunday"]')
@@ -137,5 +111,47 @@ module.exports = {
     .click('input[type="submit"]')
     .pause(5000)
     .end();
+  },
+
+  'Make Payment': function (browser) {
+    browser
+    .url('http://localhost:3000/#')
+    .waitForElementVisible('body', 1000)
+    .setValue('input[name="username"]', 'test@user13.com')
+    .setValue('input[name="password"]', 'testpw')
+    .click('button[type="submit"]')
+    .pause(5000)
+    .click('div.Select-placeholder')
+    .pause(500)
+    .click('div.Select-option.is-focused')
+    .pause(500)
+    .setValue('input#payment-amount', '200.00')
+    .pause(1000)
+    .click('[data-test="make-payment"]')
+    .pause(3000)
+    .keys('4242')
+    .pause(100)
+    .keys('4242')
+    .pause(100)
+    .keys('4242')
+    .pause(100)
+    .keys('4242')
+    .pause(500)
+    .keys('\uE004')
+    .pause(500)
+    .keys('11')
+    .keys('2020')
+    .pause(500)
+    .keys('\uE004')
+    .pause(500)
+    .keys('123')
+    .pause(500)
+    .keys('\uE004')
+    .pause(500)
+    .keys('77777')
+    .pause(500)
+    .keys('\uE007')
+    .pause(5000)
+    // Pause may need to be longer on other test clients
   }
 }
