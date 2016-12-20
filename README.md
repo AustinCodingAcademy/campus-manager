85849a44e525874812f6a1cedc4cd4fe76f8b8b7
Kevin Colten <kevincolten@gmail.com>
2016-12-20 10:56:30 -0600

    Added jsdoc

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![](http://static1.squarespace.com/static/538f3fcde4b05c5fecc7a40e/t/538f48a4e4b00d94e8c253b3/1453396632576/?format=400w)
+<img src="https://campus.austincodingacademy.com/img/aca-logo-header.jpg" width="200" />
 
 # Campus Manager
 
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 var requireDir = require('require-dir');
+var webserver = require('gulp-webserver');
+var jsdoc = require('gulp-jsdoc3');
 requireDir('./gulp');
 
 if (['test', 'production'].indexOf(process.env.NODE_ENV) === -1) {
@@ -20,6 +22,22 @@ gulp.task('build', function(callback) {
 
 gulp.task('test', function(callback) {
   runSequence('clean-db', 'nodemon-start', 'nightwatch', 'nodemon-quit', callback)
+});
+
+gulp.task('docs', function (cb) {
+  var config = require('./jsdoc.conf.json');
+  gulp.src(['./README.md', './[!node_modules]**/*.js'], {read: true})
+  .pipe(jsdoc(config, cb));
+});
+
+
+gulp.task('webserver', function() {
+  gulp.src('./docs')
+  .pipe(webserver({
+    host: '127.0.0.1',
+    port: '3001',
+    directoryListing: false
+  }));
 });
 
 gulp.task('develop', ['start', 'bundle-dev', 'sass-dev']);
