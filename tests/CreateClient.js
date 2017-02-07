9efa9512122335d281f4d3cdaa7650e00f955345
Kevin Colten <kevincolten@gmail.com>
2017-02-07 12:23:41 -0600

    updated registration test

--- tests/CreateClient.js
+++ tests/CreateClient.js
@@ -3,9 +3,10 @@ module.exports = {
     browser
     .setValue('input[name="first_name"]', 'Test')
     .setValue('input[name="last_name"]', 'Client')
-    .setValue('input[name="phone"]', '5555555555')
     .setValue('input[name="username"]', 'test@client.com')
     .setValue('input[name="password"]', 'testpw')
+    .setValue('input[name="zipcode"]', '55555')
+    .setValue('input[name="phone"]', '(555) 555-5555')
     .waitForElementVisible('button[type="submit"]', 1000)
     .click('button[type="submit"]')
     .pause(1000)
