9c301e34dcd0b3c13f2a0e8df0c4f82d044a9f02
Kevin Colten <kevincolten@gmail.com>
2017-02-16 15:48:29 -0600

    moved review links out

---
+++ src/js/data/reviews.js
@@ -0,0 +1,90 @@
+{
+  "Austin": {
+    "Google": {
+      "href": "https://www.google.com/?gws_rd=ssl#tbm=lcl&q=Austin+Coding+Academy&lrd=0x8644b4e31ad087bd:0xd6644ce54fe2531c,3,&rlfi=hd:;si:15448557169559098140;mv:!1m3!1d234.15636136085305!2d-97.7405641!3d30.268875500000007!2m3!1f0!2f0!3f0!3m2!1i1500!2i835!4f13.1",
+      "key": "austin-google"
+    },
+    "Yelp": {
+      "href": "https://www.yelp.com/writeareview/biz/lc6l_b5X8qDFFdyLFt7ySg?return_url=%2Fbiz%2Flc6l_b5X8qDFFdyLFt7ySg",
+      "key": "austin-yelp"
+    },
+    "Course Report": {
+      "href": "https://www.coursereport.com/schools/austin-coding-academy#/reviews/write-a-review",
+      "key": "austin-coursereport"
+    },
+    "SwitchUp": {
+      "href": "https://www.switchup.org/bootcamps/austin-coding-academy",
+      "key": "austin-switchup"
+    },
+    "Thinkful": {
+      "href": "https://www.thinkful.com/bootcamps/austin-coding-academy/",
+      "key": "austin-thinkful"
+    }
+  },
+  "San Antonio": {
+    "Google": {
+      "href": "https://www.google.com/?gws_rd=ssl#q=San%20Antonio%20Coding%20Academy&tbs=lf_od:-1,lf_oh:-1,lf:1,lf_ui:2,lf_pqs:EAE&rflfq=1&rlha=0&rllag=29435051,-98485887,1138&tbm=lcl&rldimm=9940786448329656884&lrd=0x865c5f51a21d2429:0x89f4c49b30b4ca34,3,",
+      "key": "sanantonio-google"
+    },
+    "Yelp": {
+      "href": "",
+      "key": "sanantonio-yelp"
+    },
+    "Course Report": {
+      "href": "",
+      "key": "sanantonio-coursereport"
+    },
+    "SwitchUp": {
+      "href": "",
+      "key": "sanantonio-switchup"
+    },
+    "Thinkful": {
+      "href": "",
+      "key": "sanantonio-thinkful"
+    }
+  },
+  "Dallas": {
+    "Google": {
+      "href": "",
+      "key": "dallas-google"
+    },
+    "Yelp": {
+      "href": "",
+      "key": "dallas-yelp"
+    },
+    "Course Report": {
+      "href": "",
+      "key": "dallas-coursereport"
+    },
+    "SwitchUp": {
+      "href": "",
+      "key": "dallas-switchup"
+    },
+    "Thinkful": {
+      "href": "",
+      "key": "dallas-thinkful"
+    }
+  },
+  "Houston": {
+    "Google": {
+      "href": "",
+      "key": "houston-google"
+    },
+    "Yelp": {
+      "href": "",
+      "key": "houston-yelp"
+    },
+    "Course Report": {
+      "href": "",
+      "key": "houston-coursereport"
+    },
+    "SwitchUp": {
+      "href": "",
+      "key": "houston-switchup"
+    },
+    "Thinkful": {
+      "href": "",
+      "key": "houston-thinkful"
+    }
+  }
+}
