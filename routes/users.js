e09edecb02e851312a2a390ccbb72e523b77facd
Kevin Colten <kevincolten@gmail.com>
2017-03-20 16:00:11 -0500

    removed insightly endpoint

--- routes/users.js
+++ routes/users.js
@@ -2,50 +2,6 @@ var express = require('express');
 var router = express.Router();
 var UserController = require('../controllers/UserController.js');
 var middleware = require('./middleware');
-const UserModel = require('../models/UserModel');
-const fetch = require('node-fetch');
-const btoa = require('btoa');
-
-router.get('/insightly', middleware.admin, (req, res) => {
-
-  const url = 'https://api.insight.ly/v2.2/Leads/';
-  const headers = {
-    Authorization: 'Basic ' + btoa(process.env.INSIGHTLY_API_KEY),
-    'Accept-Encoding': 'gzip'
-  };
-
-  function findLead(users, idx) {
-    if (idx === users.length) return;
-    const user = users[idx];
-    console.log(user.username);
-    fetch(`${url}Search?email=${user.username}`, {
-      method: 'GET',
-      headers
-    }).then(leads => {
-      return leads.json().then(leads => {
-        const lead = leads[0];
-        if (lead) {
-          user.insightly = lead['LEAD_ID'];
-          user.save(user => {
-            setTimeout(() => {
-              findLead(users, ++idx)
-            }, 201);
-          })
-        } else {
-          setTimeout(() => {
-            findLead(users, ++idx)
-          }, 201);
-        }
-      });
-    });
-  }
-
-
-  UserModel.find({}, (err, users) => {
-    findLead(users, 0);
-    return res.json(users);
-  });
-});
 
 /*
 * GET
