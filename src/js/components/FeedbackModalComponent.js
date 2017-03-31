52412455d282c9113ad66e61a2c37ace0c036a8d
Kevin Colten <kevincolten@gmail.com>
2017-03-31 09:57:07 -0500

    removed placeholder for iframe

--- src/js/components/FeedbackModalComponent.js
+++ src/js/components/FeedbackModalComponent.js
@@ -2,6 +2,19 @@ import * as React from 'react';
 import { Modal } from 'react-bootstrap';
 
 module.exports = React.createBackboneClass({
+
+  componentDidMount() {
+    window.addEventListener("message", function(e) {
+      if (typeof e.data === 'string' && e.data.indexOf('feedback-iframe') > -1) {
+        document.getElementById('feedback-iframe').className = 'hidden';
+        document.getElementById('thank-you-feedback').className = '';
+      }
+      if (typeof e.data === 'string' && e.data.indexOf('resized') > -1) {
+        document.getElementById('ratio').setAttribute('style', 'min-height: ' + e.data.split('-')[1] + 'px');
+      }
+    });
+  },
+
   render() {
     return (
       <Modal show={this.props.show} onHide={this.props.onHide}>
@@ -9,10 +22,16 @@ module.exports = React.createBackboneClass({
           <Modal.Title>Feedback</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div className="h-iframe">
-            <img className="ratio" src="https://placehold.it/8x6/ffffff/ffffff" />
-              <iframe src="/feedback" frameBorder="0">
-            </iframe>
+          <div className="iframe-wrapper" id="feedback-iframe">
+            <div className="h-iframe">
+              <div className="ratio" id="ratio" />
+              <iframe src="/feedback" frameBorder="0"></iframe>
+            </div>
+          </div>
+          <div className="hidden" id="thank-you-feedback">
+            <div className="alert alert-success text-center" role="alert">
+              <h3 style={{marginTop:'10px'}}>Thanks for your feedback!</h3>
+            </div>
           </div>
         </Modal.Body>
       </Modal>
