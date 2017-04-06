1c666a25cc484a55cba4e8e6294920a74b3cda0e
Kevin Colten <kevincolten@gmail.com>
2017-04-06 10:47:50 -0500

    fixed line endings

--- models/LocationModel.js
+++ models/LocationModel.js
@@ -2,7 +2,20 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const version = require('mongoose-version');
 
-const locationSchema = new Schema({  address: String,  name: String,  city: String,  state: String,  zipcode: String,  contact: String,  client: {    type: Schema.Types.ObjectId,    ref: 'user'  },  phone: String,  note: String}, { timestamps: true });
+const locationSchema = new Schema({
+  address: String,
+  name: String,
+  city: String,
+  state: String,
+  zipcode: String,
+  contact: String,
+  client: {
+    type: Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  phone: String,
+  note: String
+}, { timestamps: true });
 
 locationSchema.plugin(version, { collection: 'locations__versions' });
 
--- models/TextbookModel.js
+++ models/TextbookModel.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const version = require('mongoose-version');
 
-const textbookSchema = new Schema({  name: String,  instructor_url: String,  student_url: String,  client: {    type: Schema.Types.ObjectId,    ref: 'user'  }}, { timestamps: true });
+const textbookSchema = new Schema({
+  name: String,
+  instructor_url: String,
+  student_url: String,
+  client: {
+    type: Schema.Types.ObjectId,
+    ref: 'user'
+  }
+}, { timestamps: true });
 
 textbookSchema.plugin(version, { collection: 'textbooks__versions' });
 
