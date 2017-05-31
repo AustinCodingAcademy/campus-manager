ed96c5c06a3ade954b404023d54a5b277aefddaf
Kevin Colten <kevincolten@gmail.com>
2017-05-31 10:44:06 -0500

    enroll in future courses in track

--- src/js/collections/TracksCollection.js
+++ src/js/collections/TracksCollection.js
@@ -3,5 +3,21 @@ const TrackModel = require('../models/TrackModel');
 
 module.exports = Backbone.Collection.extend({
   url: 'api/tracks',
-  model: TrackModel
+  model: TrackModel,
+  comparator: (a, b) => {
+    const aTerm = a.get('courses').pluck('term')[0];
+    const bTerm = b.get('courses').pluck('term')[0];
+    if (aTerm && bTerm) {
+      const aStartDate = aTerm.get('start_date');
+      const bStartDate = bTerm.get('start_date');
+      if (aStartDate === bTerm) {
+        if (aTerm.get('name') === bTerm.get('name')) {
+          return 0;
+        }
+        return aTerm.get('name') > bTerm.get('name') ? 1 : -1;
+      }
+      return aStartDate > bStartDate ? 1 : -1;
+    }
+    return 1;
+  }
 });
