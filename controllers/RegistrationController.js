926faa7f464fae9aed5a7c72016a1e938965bc9b
Kevin Colten <kevincolten@gmail.com>
2017-06-06 14:16:51 -0500

    setting first price

--- controllers/RegistrationController.js
+++ controllers/RegistrationController.js
@@ -1,5 +1,6 @@
 const CourseModel = require('../models/CourseModel');
-const TrackModel = require('../models/TrackModel.js');
+const TrackModel = require('../models/TrackModel');
+const UserModel = require('../models/UserModel');
 const moment = require('moment');
 
 /**
@@ -16,23 +17,33 @@ module.exports = {
     function registerUser(courses, idx) {
       idx = idx || 0;
       const course = courses[idx];
-      const userIdx = course.registrations.indexOf(req.body.userId);
-      if (userIdx === -1) {
-        course.registrations.set(course.registrations.length, req.body.userId);
-      }
-      course.save(err => {
-        if (err) {
-          return res.json(500, {
-            message: 'Error saving registration',
-            error: err
+      UserModel.findOne({
+        _id: req.body.userId,
+        client: req.user.client
+      }, (err, user) => {
+        if (!user.price) {
+          user.price = course.cost;
+        }
+        user.save((err, user) => {
+          const userIdx = course.registrations.indexOf(req.body.userId);
+          if (userIdx === -1) {
+            course.registrations.set(course.registrations.length, req.body.userId);
+          }
+          course.save(err => {
+            if (err) {
+              return res.json(500, {
+                message: 'Error saving registration',
+                error: err
+              });
+            }
+            if (idx < courses.length - 1) {
+              registerUser(courses, ++idx)
+            } else {
+              return res.json(req.body.userId);
+            }
           });
-        }
-        if (idx < courses.length - 1) {
-          registerUser(courses, ++idx)
-        } else {
-          return res.json(req.body.userId);
-        }
-      })
+        });
+      });
     }
 
     function singleCourse() {
