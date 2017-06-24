7f902191c63b60c637d5f6dc62028257ae62ee79
Kevin Colten <kevincolten@gmail.com>
2017-06-24 15:08:54 -0500

    updated test

--- tests/CreateClient.js
+++ tests/CreateClient.js
@@ -1,11 +1,11 @@
 module.exports = {
   'Create Client': browser => {
     browser
+    .click('#campus-select option[value="Austin"]')
     .setValue('input[name="first_name"]', 'Test')
     .setValue('input[name="last_name"]', 'Client')
     .setValue('input[name="username"]', 'test@client.com')
     .setValue('input[name="password"]', 'testpw')
-    .setValue('input[name="zipcode"]', '55555')
     .setValue('input[name="phone"]', '(555) 555-5555')
     .waitForElementVisible('button[type="submit"]', 1000)
     .click('button[type="submit"]')
