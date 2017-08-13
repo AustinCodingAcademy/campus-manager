f1099702cf3bbf705dd1b5f9820a9f11f8d0b438
stackbaxter <stackbaxter@yahoo.com>
2017-08-13 03:47:31 -0500

    courses tester file

---
+++ courses.js
@@ -0,0 +1,44 @@
+let courses = [
+  {
+    location: "Dallas",
+    days: ["Mon", "Wed"],
+    course: "Full-Stack",
+    startDate: new dateTime(9/15/17)
+  },
+  {
+    location: "San Antonio",
+    days: ["Mon","Tue","Wed","Thu"],
+    course: "Dot-Net",
+    startDate: new dateTime(9/15/17)
+  },
+  {
+    location: "San-Antonio",
+    days: ["Mon", "Wed"],
+    course: "Full-Stack",
+    startDate: new dateTime(9/15/17)
+  },
+  {
+    location: "Austin",
+    days: ["Tue","Thu"],
+    course: "Web-VR",
+    startDate: new dateTime(9/15/17)
+  },
+  {
+    location: "Dallas",
+    days: ["Tue","Thu"],
+    course: "Web-VR",
+    startDate: new dateTime(9/15/17)},
+  {
+    location: "Austin",
+    days: ["Tue","Thu"],
+    course: "Full-Stack",
+    startDate: new dateTime(9/15/17)},
+  {
+    location: "Austin",
+    days: ["Mon", "Wed","Tue","Thu"],
+    course: "Full-Stack",
+    startDate: new dateTime(9/15/17)
+  },
+  
+
+]
